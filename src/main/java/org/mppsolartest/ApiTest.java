545d45a665a8b1a9b0b1aa67fe4f06235e9de94e
Synogen <synogen@freenet.de>
2024-03-04 19:28:25 +0100

    Add API test

---
+++ src/main/java/org/mppsolartest/ApiTest.java
@@ -0,0 +1,36 @@
+package org.mppsolartest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fazecast.jSerialComm.SerialPort;
+import org.mppsolartest.command.Qpiri;
+import org.mppsolartest.communication.SerialHandler;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class ApiTest {
+    public static void main(String[] args) throws Exception {
+        var config = new Properties();
+        config.load(new FileReader("api.properties"));
+        var port = SerialPort.getCommPort(config.getProperty("port"));
+        port.setBaudRate(2400);
+        if (port.openPort()) {
+            System.out.println(port.getSystemPortName() + " open");
+            var serialHandler = new SerialHandler(port);
+
+            port(80);
+            get("/api/rawcommand/:cmd", (request, response) -> {
+                var command = request.params("cmd").toUpperCase();
+                return serialHandler.excuteSimpleCommand(command);
+            });
+            get("/api/command/:cmd", (request, response) -> {
+                var command = request.params("cmd").toUpperCase();
+                if (command.equalsIgnoreCase("QPIRI")) return new ObjectMapper().writeValueAsString(new Qpiri().run(serialHandler));
+                return "Unknown or unimplemented command " + command;
+            });
+        }
+    }
+}
