f0ff46c113b8535c1f86e17809ebcb02d1eeee4e
Synogen <synogen@freenet.de>
2024-03-12 22:24:25 +0100

    CLean SerialHandler a bit

--- src/main/java/org/mppsolartest/serial/SerialHandler.java
+++ src/main/java/org/mppsolartest/serial/SerialHandler.java
@@ -9,20 +9,20 @@ public class SerialHandler {
     private final InputStream input;
     private final OutputStream output;
     private final SerialPort serialPort;
-    private int _errorcount = 0;
+    private int errorcount = 0;
 
     private void clearbuffer() {
         try {
             for(int buflen = this.input.available(); buflen > 0; --buflen) {
                 this.input.read();
             }
-        } catch (Exception var2) {
-            var2.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
     }
 
-    public SerialHandler(SerialPort serialPort) throws Exception {
+    public SerialHandler(SerialPort serialPort) {
         this.serialPort = serialPort;
         this.serialPort.setComPortParameters(2400, 8, 1, 0);
         this.serialPort.setComPortTimeouts(1, 1200, 0);
@@ -45,13 +45,13 @@ public class SerialHandler {
                 this.output.flush();
                 long end = System.currentTimeMillis() + 3000L;
                 StringBuilder sb = new StringBuilder();
-                boolean flag = false;
+                boolean linebreak = false;
 
                 while(System.currentTimeMillis() < end) {
                     int ch;
                     if ((ch = this.input.read()) >= 0) {
                         if (ch == 13) {
-                            flag = true;
+                            linebreak = true;
                             break;
                         }
 
@@ -59,7 +59,7 @@ public class SerialHandler {
                     }
                 }
 
-                if (!flag) {
+                if (!linebreak) {
                     result = false;
                 }
 
@@ -70,7 +70,7 @@ public class SerialHandler {
                     returnValue = "";
                 }
             }
-        } catch (Exception var15) {
+        } catch (Exception e) {
             result = false;
         } finally {
             this.countErrorandNotifyProcesser(result);
@@ -99,13 +99,13 @@ public class SerialHandler {
 
                 long end = System.currentTimeMillis() + 10000L;
                 StringBuilder sb = new StringBuilder();
-                boolean flag = false;
+                boolean linebreak = false;
 
                 while(System.currentTimeMillis() < end) {
                     int ch;
                     if ((ch = this.input.read()) >= 0) {
                         if (ch == 13) {
-                            flag = true;
+                            linebreak = true;
                             break;
                         }
 
@@ -113,7 +113,7 @@ public class SerialHandler {
                     }
                 }
 
-                if (!flag) {
+                if (!linebreak) {
                     if (command.indexOf("P") == 0) {
                         System.out.println("4567890::" + returnValue);
                     }
@@ -130,7 +130,7 @@ public class SerialHandler {
                     }
                 }
             }
-        } catch (Exception var17) {
+        } catch (Exception e) {
             result = false;
         } finally {
             this.countErrorandNotifyProcesser(result);
@@ -141,13 +141,13 @@ public class SerialHandler {
 
     private void countErrorandNotifyProcesser(boolean success) {
         if (success) {
-            this._errorcount = 0;
+            this.errorcount = 0;
         } else {
-            ++this._errorcount;
+            ++this.errorcount;
         }
 
-        if (this._errorcount >= 12) {
-            System.out.println("---------communication exception---------" + this._errorcount);
+        if (this.errorcount >= 12) {
+            System.out.println("---------communication exception---------" + this.errorcount);
         }
 
     }
@@ -170,108 +170,5 @@ public class SerialHandler {
                 this.serialPort.closePort();
             } catch (Exception ignored) {}
         }
-    }
-
-    public String getDeviceName() {
-        String portName = this.serialPort.getSystemPortName();
-        int index = portName.lastIndexOf("/");
-        if (index > 0) {
-            portName = portName.substring(index + 1);
-        }
-
-        return portName;
-    }
-
-    public int getMpptTrackNumber() {
-        int mpptTrackNumber = 2;
-
-        try {
-            String result = this.excuteCommand("QPIRI", true);
-            System.out.println("result===" + result);
-            if (result != null && !"".equals(result) && !result.equals("QPIRI")) {
-                String[] arr = result.split(" ");
-                mpptTrackNumber = Integer.parseInt(arr[7]);
-            }
-        } catch (Exception var4) {
-            var4.printStackTrace();
-        }
-
-        return mpptTrackNumber;
-    }
-
-    public String getSerialNo() {
-        String serialno = "";
-        String serialnoStr;
-
-        try {
-            for(var i = 0; i < 2; ++i) {
-                serialnoStr = this.excuteCommand("QSID", true);
-                if (serialnoStr != null && !"".equals(serialnoStr) && !serialnoStr.equalsIgnoreCase("(NAK") && !serialnoStr.equalsIgnoreCase("(ACK") && !serialnoStr.equals("QSID")) {
-                    String validLen = serialnoStr.substring(1, 3);
-                    serialno = serialnoStr.substring(3, 3 + Integer.parseInt(validLen));
-                    break;
-                }
-            }
-
-            if (serialno.length() > 0) {
-                return serialno;
-            }
-
-            for(var i = 0; i < 3; ++i) {
-                serialnoStr = this.excuteCommand("QID", true);
-                if (serialnoStr != null && !"".equals(serialnoStr) && !serialnoStr.equalsIgnoreCase("(NAK") && !serialnoStr.equalsIgnoreCase("(ACK") && !serialnoStr.equals("QID")) {
-                    serialno = serialnoStr.substring(1);
-                    break;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return serialno;
-    }
-
-    public String getModeType() {
-        String machineTypeStr = "";
-
-        try {
-            String qpiriStr = this.excuteCommand("QPIRI", true);
-            System.out.println(" qpiriStr===" + qpiriStr);
-            if (!"".equals(qpiriStr) && !qpiriStr.equals("(NAK") && !qpiriStr.equals("QPIRI")) {
-                String[] ratingInfo = qpiriStr.split(" ");
-                machineTypeStr = ratingInfo[8];
-                return machineTypeStr;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return machineTypeStr;
-    }
-
-    public String getDeviceModel() {
-        String deviceModel = "";
-
-        try {
-            String qdmStr = this.excuteCommand("QDM", true);
-            if (!"".equals(qdmStr) && !qdmStr.equals("(NAK") && !qdmStr.equals("QDM")) {
-                qdmStr = qdmStr.substring(1);
-                deviceModel = qdmStr;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return deviceModel;
-    }
-
-    public boolean isSupportQCTH() {
-        String qchtStr = this.excuteCommand("QCHT", true);
-        return qchtStr != null && !"".equals(qchtStr) && !qchtStr.equals("(NAK");
-    }
-
-    public boolean isSupportQPPS() {
-        String ppsStr = this.excuteCommand("QPPS", true);
-        return ppsStr != null && !"".equals(ppsStr) && !ppsStr.equals("(NAK");
     }
 }
