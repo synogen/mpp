033abe370022511e55692cf28ed6ff70324ab76d
Synogen <synogen@freenet.de>
2024-03-15 02:15:56 +0100

    Test class for quick debug/testing

---
+++ src/main/test/org/mppsolartest/CommandTest.java
@@ -0,0 +1,25 @@
+package org.mppsolartest;
+
+import org.mppsolartest.command.Qpiri;
+import org.mppsolartest.model.Field;
+
+import java.math.BigDecimal;
+
+/**
+ * Only for debug/testing purposes
+ */
+public class CommandTest {
+
+    private static String qpiriExample = "(230.0 21.7 230.0 50.0 21.7 5000 5000 48.0 47.0 46.5 57.0 57.0 9 002 070 1 2 1 1 01 0 0 54.0 0 1";
+    public static void main(String[] args) {
+        var test = new Qpiri().parseResponse(qpiriExample);
+
+        var f = new Field<>("AC input voltage in V", BigDecimal::new);
+
+        System.out.println("Test");
+
+
+    }
+
+
+}
